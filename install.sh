c1a28c62c9a73705ffce9e7b14315e4656e96ea4
Hamburger <72227790+bwrbur@users.noreply.github.com>
2023-07-01 11:27:59 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+sudo systemctl stop iptables
+sudo systemctl disable iptables
+sudo iptables -F
+sudo iptables -X
+sudo iptables -Z
+sudo iptables -P INPUT ACCEPT
+sudo iptables -P FORWARD ACCEPT
+sudo iptables -P OUTPUT ACCEPT
+sudo ufw allow 22/tcp
+sudo ufw allow 80/tcp
+sudo ufw allow 443/tcp
+sudo ufw enable
