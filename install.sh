ad2de9432d748bc91d8b187453c058ef01574320
Hamburger <72227790+bwrbur@users.noreply.github.com>
2023-07-15 23:03:14 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,12 @@
 
 echo "Starting installation.."
 sleep 3
+
 echo "Configuring updates.."
 sleep 2
 sudo apt update
 sleep 3
+
 echo "Configuring iptables.."
 sleep 2
 sudo systemctl stop iptables
@@ -17,6 +19,7 @@ sudo iptables -P INPUT ACCEPT
 sudo iptables -P FORWARD ACCEPT
 sudo iptables -P OUTPUT ACCEPT
 sleep 3
+
 echo "Configuring ufw.."
 sleep 2
 sudo ufw allow 22/tcp
@@ -24,10 +27,12 @@ sudo ufw allow 80/tcp
 sudo ufw allow 443/tcp
 sudo ufw enable
 sleep 3
+
 echo "Installing neofetch.."
 sleep 2
 sudo apt-get install neofetch -y
 sleep 3
 neofetch
 sleep 2
+
 echo "Installation has been successfully completed!"
