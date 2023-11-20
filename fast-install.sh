47f780e597e256ff2a957ab64ac2b26f1046238f
Hamburger <72227790+bwrbur@users.noreply.github.com>
2023-11-20 23:57:43 +0000

    Update fast-install.sh

--- fast-install.sh
+++ fast-install.sh
@@ -27,7 +27,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | sudo 
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-echo "Installing Node LTS.."
+echo "Installing Node [LTS].."
 nvm install lts
 
 echo "Installing neofetch.."
