41b588b0ac5c953a93587e8778b9a0d08b918186
Hamburger <72227790+bwrbur@users.noreply.github.com>
2023-11-20 23:57:55 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -35,7 +35,7 @@ export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || pr
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 sleep 3
 
-echo "Installing Node LTS.."
+echo "Installing Node [LTS].."
 sleep 2
 nvm install lts
 sleep 3
