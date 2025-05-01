fc6807f1e33402db3411c1dd3cac77514ad12446
nitcord <72227790+nitcord@users.noreply.github.com>
2025-05-01 07:18:09 +0100

    Update

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ sudo ufw --force limit 22/tcp
 sudo ufw --force enable
 
 echo "Installing nvm.."
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | sudo bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | sudo bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
